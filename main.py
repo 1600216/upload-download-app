0e3fd01b9b657603ad7c857e138095ca630ed0e7
1600216 <1600216gold@gmail.com>
2023-07-26 12:35:31 +0500

    1111

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template, send_file
+from flask_sqlalchemy import SQLAlchemy
+from io import BytesIO
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qwerty.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        upload = Upload(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+        return f'Uploaded: {file.filename}'
+    return render_template('index.html')
+
+
+@app.route('/download/<upload_id>')
+def download(upload_id):
+    upload = Upload.query.filter_by(id=upload_id).first()
+    return send_file(BytesIO(upload.data), download_name=upload.filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
